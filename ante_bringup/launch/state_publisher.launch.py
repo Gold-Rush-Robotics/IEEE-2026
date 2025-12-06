d9d8e876d239b5c05f956edc86dafbd276f62048
Sean Devlin <sean.dev224@gmail.com>
2025-12-06 01:30:43 +0000

    joint state publisher working with rviz on drivebase

---
+++ ante_bringup/launch/state_publisher.launch.py
@@ -0,0 +1,41 @@
+import os
+import xacro
+
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python import get_package_share_path
+
+def generate_launch_description():
+    urdf_path = os.path.join(get_package_share_path("ante_description"), "robots", "ante.urdf.xacro")
+    rviz_config_path = os.path.join(get_package_share_path("ante_bringup"), "config", "rviz_view_config.rviz")
+
+    # print(rviz_config_path)
+
+    urdf = ParameterValue(Command(["xacro ", urdf_path]), value_type=str)
+    # Command(["xacro ", urdf_path, " > /workspace/DevEnv/jazzy_ws/src/IEEE-2026/ante_description/generated_urdf.urdf"])
+    # urdf = xacro.process_file(urdf_path).toprettyxml()
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": urdf}]
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=[{"-d", rviz_config_path}]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        rviz2_node,
+        joint_state_publisher_gui_node
+    ])
